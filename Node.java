1ea92de2a3f5c0d241ca61f7d9fee1f7e88275e6
rafiadpr <work.rafiadrianp@gmail.com>
2025-06-07 20:42:40 +0700

    [UPDATE] (QueueTransaksi, Node) : add logic for add transaksi and node

---
+++ Node.java
@@ -0,0 +1,11 @@
+
+public class Node {
+
+    Kendaraan data;
+    Node next;
+
+    public Node(Kendaraan data) {
+        this.data = data;
+        this.next = null;
+    }
+}
---
+++ QueueTransaksi.java
@@ -0,0 +1,28 @@
+
+public class QueueTransaksi {
+
+    TransaksiPengisian[] transaksi;
+    int front, rear, size;
+
+    public QueueTransaksi(int kapasitas) {
+        transaksi = new TransaksiPengisian[kapasitas];
+        front = rear = 0;
+        size = 0;
+    }
+
+    public void tambahTransaksi(TransaksiPengisian t) {
+        if (size < transaksi.length) {
+            transaksi[rear++] = t;
+            size++;
+        } else {
+            System.out.println("Transaksi penuh!");
+        }
+    }
+
+    public void tampilkanRiwayat() {
+        for (int i = 0; i < size; i++) {
+            transaksi[i].tampilkanTransaksi();
+            System.out.println("-----------");
+        }
+    }
+}
